acb461c0bb65e5b9a1b34b9c143911893b8fe617
rem3_1415926 <d4rksh4dy@gmail.com>
2026-05-17 17:32:51 +0200

    some prototype panel cleanup

--- data_analysis/src/gui_stuff/panel_test3.py
+++ data_analysis/src/gui_stuff/panel_test3.py
@@ -43,76 +43,21 @@ def make_plot(params: list = [], start = None, stop = None):
 
     fig.update_layout({
         "xaxis": {"title": "Time"},
-        # "yaxis": {"title": (params[0] if params else "")},  
-        # ^ fixes a layout issue, no idea why
         "legend": {"orientation": "h"},
-        # "autosize": True,
         "margin": {"l": 0, "r": 0, "t": 0, "b": 0},
+        "height": 400,
     }, template=None)
     return fig
 
-
-def add_plot(event):
-    # global plot_layouts
+def make_plot_panel(rm_btn: pn.widgets.Button):
+    global df
     param_widget = pn.widgets.MultiChoice(name="parameter", value=["batt_volt", "batt_curr"], options=list(df.columns), sizing_mode="stretch_width")
+    top_row = pn.Row(param_widget, rm_btn)
     dt_start = pn.widgets.DatetimePicker(name="From", value=df.index.min(), sizing_mode="stretch_width")
     dt_stop  = pn.widgets.DatetimePicker(name="To",   value=df.index.max(), sizing_mode="stretch_width")
     dt_range = pn.Row(dt_start, dt_stop, sizing_mode="stretch_width")
-    # bound_plot = pn.bind(make_plot, params=param_widget, start=dt_start, stop=dt_stop)
     bound_plot = pn.pane.Plotly(
         pn.bind(make_plot, params=param_widget, start=dt_start, stop=dt_stop),
         sizing_mode="stretch_width",config={"responsive": True})
-    # plot_layouts.append([param_widget, dt_range, bound_plot])
-    # render()
-    global dynamic
-    # dynamic.extend([dt_range, bound_plot])
-    # dynamic.append(pn.Column(param_widget, dt_range, bound_plot, width=500, sizing_mode="fixed"))
-    dynamic.append(pn.Column(param_widget, dt_range, bound_plot, sizing_mode="stretch_width"))
 
-
-main_layout = []
-plot_layouts = []
-
-add_plot_btn = pn.widgets.Button(name="add plot")
-add_plot_btn.on_click(add_plot)
-
-
-# def render():
-#     global main_layout
-#     main_layout = []
-#     main_layout.append(add_plot_btn)
-#     for pl in plot_layouts:
-#         main_layout.extend(pl)
-
-
-# render()
-
-
-# # sideways button:
-# btn = pn.widgets.Button(name="Add panel", button_type="primary")
-# btn.styles = {
-#     "writing-mode": "vertical-rl", "transform": "rotate(180deg)",
-#     "height": "120px", "width": "40px",
-# }
-# main_layout.append(btn)
-
-
-container = pn.Column(sizing_mode="stretch_width")
-main_layout = pn.Row(container, sizing_mode="stretch_width")
-
-container.append(add_plot_btn)
-dynamic = pn.Column()
-container.append(dynamic)
-
-template = pn.template.MaterialTemplate(
-    site="Panel",
-    title="Panel Test 3",
-    main=main_layout,
-)
-# template.main.styles = {
-#     "width": "100vw",
-#     "max-width": "100vw",
-#     "padding": "0",
-#     "margin": "0",
-# }
-template.servable()
+    return pn.Column(top_row, dt_range, bound_plot, sizing_mode="stretch_width")
--- data_analysis/src/panel_gui_main.py
+++ data_analysis/src/panel_gui_main.py
@@ -9,9 +9,8 @@ import panel as pn
 import plotly.graph_objects as go
 pn.extension('plotly')
 
-from gui_stuff.panel_test3 import make_plot
+from gui_stuff.panel_test3 import make_plot_panel
 
-# import backend.dynamic #as volatile
 import backend.dynamic as volatile
 
 ROOT = Path(__file__).parent
@@ -23,21 +22,15 @@ def get_data() -> pd.DataFrame:
 df = get_data()
 
 
-le_counter = 0
-
 css_vbutton = {
     "writing-mode": "vertical-rl", "transform": "rotate(180deg)",
     # "height": "120px", "width": "20px",
 }
 
 def reload_backend(event):
-    # global volatile
     print(f"was {volatile.dynamic_1.dynamic_function()}")
     importlib.reload(volatile)
     print(f"is  {volatile.dynamic_1.dynamic_function()}")
-    # print(f"was {backend.dynamic.dynamic_1.dynamic_function()}")
-    # importlib.reload(backend.dynamic)
-    # print(f"is  {backend.dynamic.dynamic_1.dynamic_function()}")
 
 def local_dyncamic_func():
     return volatile.dynamic_1.dynamic_function()
@@ -50,7 +43,6 @@ header_layout = pn.Column(sizing_mode="stretch_width")
 
 reload_backend_btn = pn.widgets.Button(name="reload backend scripts")
 reload_backend_btn.on_click(reload_backend)
-header_layout.append(reload_backend_btn)
 header_layout.append(pn.widgets.StaticText(value=local_dyncamic_func()))
 
 
@@ -59,30 +51,21 @@ header_layout.append(pn.widgets.StaticText(value=local_dyncamic_func()))
 # Column that can be extended, containing Rows that can also be extended
 extendable_column = pn.Column()
 def add_row(event):
-    global le_counter
-    le_counter += 1
     def add_panel(event):
-        global le_counter
-        le_counter += 1
         def rm_panel(event):
             extendable_row.remove(panel)
         panel = pn.Column()
         panel.styles = {'border': '1px solid black'}
-        rm_panel_btn = pn.widgets.Button(name=f"remove panel {le_counter}")
+        rm_panel_btn = pn.widgets.Button(name=f"remove panel")
         rm_panel_btn.on_click(rm_panel)
-        panel.append(rm_panel_btn)
-        panel.append(pn.widgets.StaticText(value=local_dyncamic_func()))
-        # funny_btn = pn.widgets.Button(name=f"le funny {local_dyncamic_func()}")
-        # funny_btn.on_click(local_dyncamic_print)
-        # panel.append(funny_btn)
-        panel.append(make_plot_panel())
+        panel.append(make_plot_panel(rm_panel_btn))
         extendable_row.append(panel)
     def rm_row(event):
         extendable_column.remove(main_row)
-    rm_row_btn = pn.widgets.Button(name=f"remove row {le_counter}")
+    rm_row_btn = pn.widgets.Button(name=f"remove row")
     rm_row_btn.styles = css_vbutton
     rm_row_btn.on_click(rm_row)
-    add_panel_btn = pn.widgets.Button(name=f"Add panel {le_counter}+1")
+    add_panel_btn = pn.widgets.Button(name=f"Add panel")
     add_panel_btn.styles = css_vbutton
     add_panel_btn.on_click(add_panel)
     extendable_row = pn.Row()
@@ -93,17 +76,6 @@ def add_row(event):
     main_row.append(add_panel_btn)
     extendable_column.append(main_row)
 
-def make_plot_panel():
-    global df
-    param_widget = pn.widgets.MultiChoice(name="parameter", value=["batt_volt", "batt_curr"], options=list(df.columns), sizing_mode="stretch_width")
-    dt_start = pn.widgets.DatetimePicker(name="From", value=df.index.min(), sizing_mode="stretch_width")
-    dt_stop  = pn.widgets.DatetimePicker(name="To",   value=df.index.max(), sizing_mode="stretch_width")
-    dt_range = pn.Row(dt_start, dt_stop, sizing_mode="stretch_width")
-    bound_plot = pn.pane.Plotly(
-        pn.bind(make_plot, params=param_widget, start=dt_start, stop=dt_stop),
-        sizing_mode="stretch_width",config={"responsive": True})
-
-    return pn.Column(param_widget, dt_range, bound_plot, sizing_mode="stretch_width")
 
 
 # MAIN LAYOUT =================================================================
@@ -115,11 +87,23 @@ main_layout.append(header_layout)
 main_layout.append(extendable_column)
 main_layout.append(add_row_btn)
 
+sidebar=[]
+sidebar.append(reload_backend_btn)
+sidebar.append(pn.widgets.StaticText(value="Timestamp tags?"))
+sidebar.append(pn.widgets.StaticText(value="add now [label]"))
+sidebar.append(pn.widgets.StaticText(value="add any [label] [datetime]"))
+sidebar.append(pn.widgets.StaticText(value="find by name"))
+sidebar.append(pn.widgets.StaticText(value="> copy found as timestamp"))
+sidebar.append(pn.widgets.StaticText(value="> delete found"))
+# * do the "facebook delete": mark as deleted, hide as if deleted, but do 
+# not actually remove from dataset.
+
 
 # HOSTING =====================================================================
 template = pn.template.MaterialTemplate(
     site="Panel",
     title="Dynamic Panel",
+    sidebar=sidebar,
     main=main_layout,
 )
 template.servable()
